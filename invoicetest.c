b5c8ac086374d39c7cdec499cbb1b7d33d7ddb42
Sebastian Schulze <sebastian.schulze@education-siemens.com>
2013-02-24 11:13:20 +0100

    task to work

---
+++ invoicetest.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   // variables declaration
+
+   //input
+
+   // calculation resistors
+
+   // calculation coils
+
+   // calculation capacitors
+
+   // print out
+
+   return 0;
+}
